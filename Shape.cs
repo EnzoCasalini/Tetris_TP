23a681be2b076e476f852f445cc9bcef2c846269
Enzo <enzocasa83@gmail.com>
2022-02-23 20:40:32 +0100

    Ajout du tp Tetris

---
+++ Shape.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Drawing;
+
+namespace Tetris_Toxiques
+{
+	public class Shape
+	{
+		// Les formes que nous créerons auront une hauteur et une largeur. Il faut aussi un tableau de case pour savoir quelles cases sont remplies par la forme.
+		public int width;
+		public int height;
+		public int[,] cases;
+		private int[,] backupCases;
+
+		// Gestion de la couleur.
+		public Brush color;
+
+		// Cette méthode nous permettra de tourner la forme.
+		public void turn()
+        {
+			// On fait une copie du tableau contenant les 0 et 1 de la forme afin de pouvoir revenir à la disposition précédente en cas de problème.
+			backupCases = cases;
+
+			cases = new int[width,height];
+			for (int i = 0; i < width; i++)
+            {
+				for (int j = 0; j < height; j++)
+                {
+					// Ici, j'ai effectué de nombreux de tests pour bien comprendre ce qu'il se passait.
+					// Déjà, on inverse i et j dans le cases[j,i] parce que, une fois la rotation effectuée, la largeur et la hauteur sont inversées.
+					// Après, on peut mettre différentes choses dans le backupCases en fonction de ce qu'on souhaite faire, ici on faire une rotation vers la droite.
+					// Par exemple, si on veut faire une rotation sur la gauche, il faudrait avoir ça : backupCases[i, width - 1 - j];
+					cases[i, j] = backupCases[height - 1 - j, i];
+                }
+            }
+
+			// On inverse la hauteur et la largeur puisqu'on a fait tourner la forme.
+			var temp = height;
+			height = width;
+			width = temp;
+        }
+
+		// Cette méthode nous permettra de revenir en arrière si jamais le joueur veut tourner la forme mais que c'est impossible.
+		public void rollback()
+        {
+			cases = backupCases;
+
+			var temp = height;
+			height = width;
+			width = temp;
+        }
+	}
+}
+
---
+++ ShapesHandler.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Drawing;
+
+namespace Tetris_Toxiques
+{
+    // On met en static pour que le constructor soit forcément de type ShapesHandler.
+    static class ShapesHandler
+    {
+        private static Shape[] shapesArray;
+        
+        // Tableau qui contient différentes couleurs pour l'aléatoire.
+        //private static Brush[] colors = { Brushes.Lime, Brushes.Yellow, Brushes.Red, Brushes.DarkViolet, Brushes.Aqua, Brushes.DeepPink, Brushes.Snow };
+
+        // Mettre le constructeur en static permet de forcer le type, ici ShapesHandler.
+        static ShapesHandler()
+        {
+            // On crée les différents types de formes que l'on ajoute dans notre tableau shapesArray.
+            shapesArray = new Shape[]
+            {
+                // Le carré.
+                new Shape
+                {
+                    width = 2,
+                    height = 2,
+                    cases = new int[,]
+                    {
+                        { 1, 1 },
+                        { 1, 1 }
+                    },
+                    color = Brushes.Red
+                },
+                // La barre verticale / horizontale.
+                new Shape
+                {
+                    width = 1,
+                    height = 4,
+                    cases = new int[,]
+                    {
+                        { 1 },
+                        { 1 },
+                        { 1 },
+                        { 1 }
+                    },
+                    color = Brushes.Lime
+                },
+                // La couronne.
+                new Shape
+                {
+                    width = 3,
+                    height = 2,
+                    cases = new int[,]
+                    {
+                        { 0, 1, 0 },
+                        { 1, 1, 1 }
+                    },
+                    color = Brushes.DeepSkyBlue
+                },
+                // Les L.
+                new Shape
+                {
+                    width = 3,
+                    height = 2,
+                    cases = new int[,]
+                    {
+                        { 0, 0, 1 },
+                        { 1, 1, 1 }
+                    },
+                    color = Brushes.Purple
+                },
+                new Shape
+                {
+                    width = 3,
+                    height = 2,
+                    cases = new int[,]
+                    {
+                        { 1, 0, 0 },
+                        { 1, 1, 1 }
+                    },
+                    color = Brushes.Yellow
+                },
+                // Les serpents.
+                new Shape
+                {
+                    width = 3,
+                    height = 2,
+                    cases = new int[,]
+                    {
+                        { 1, 1, 0 },
+                        { 0, 1, 1 }
+                    },
+                    color = Brushes.DeepPink
+                },
+                new Shape
+                {
+                    width = 3,
+                    height = 2,
+                    cases = new int[,]
+                    {
+                        { 0, 1, 1 },
+                        { 1, 1, 0 }
+                    },
+                    color = Brushes.DarkOrange
+                }
+            };
+        }
+
+        // Cette méthode permet de renvoyer une forme aléatoire du tableau créé juste au-dessus.
+        public static Shape GetRandomShape()
+        {
+            var shape = shapesArray[new Random().Next(shapesArray.Length)];
+            /*shape.color = GetColorRandomly();*/
+            return shape;
+        }
+
+        /*public static Brush GetColorRandomly()
+        {
+            Random rnd = new Random();
+            int rand = rnd.Next(0, colors.Length);
+            return colors[rand];
+        }*/
+    }
+}
+
